77ff79ec6dc6e9f6512d458913ce3ca6b16bb652
kayinfinite <123296312+kayinfinite@users.noreply.github.com>
2025-06-02 16:55:26 +0300

    Tema updatea
    continuare

--- 20_05_25/Matrici.cpp
+++ 20_05_25/Matrici.cpp
@@ -4,36 +4,31 @@
 using namespace std;
 
 unsigned int Mat[10][10], n = 0;
-bool orientat;
 
-void Citeste(unsigned n, bool orientat, unsigned Mat[10][10]){
-    cout<<"Numarul de elemente: "; cin>>n; cout << n;
-    for(int i = 1; i<=n; i++)
+
+void CitesteTast(unsigned n, unsigned Mat[10][10]){
+    bool orientat;
+    cout << "Graf Orientat?";
+    cin >> orientat;
+    for(int i = 0; i<n; i++)
     {
-        cout << "i= "<< i <<'\n';
-        for(int j = 1; j<=n; j++)
+        for(int j = 0; j<n; j++)
         {
-            cout << "i= "<< i << " j = " << j<<'\n';
             if(orientat){ //citire pentru graf orientat
                 if(j != i){
-                cout << "Nodul " << i <<" e conectat cu nodul " << j << "  or: ";
+                cout << "Nodul " << i <<" e conectat cu nodul " << j << ": ";
                 cin >> Mat[i][j];  
                 }else{ //diagonala principala va fi mereu 0
                 Mat[i][j] = 0;
                 }
             }else{
                 if(j > i){ //citire pentru graf neorientat
-                    cout << "i= "<< i <<'\n';
-                    cout << "Nodul " << i <<" e conectat cu nodul " << j << " neor: ";
+                    cout << "Nodul " << i <<" e conectat cu nodul " << j << ": ";
                     cin >> Mat[i][j];
-                    Mat[j][i] = Mat[i][j];
-                    
+                    Mat[j][i] = Mat[i][j];   
                 } 
-                if(i == j){//diagonala principala va fi mereu 0
+                if(i == j)//diagonala principala va fi mereu 0
                     Mat[i][j] = 0;
-                    
-                }
-                cout << "i= "<< i <<'\n';
             }
              
         }   
@@ -42,18 +37,27 @@ void Citeste(unsigned n, bool orientat, unsigned Mat[10][10]){
 
 void Scriere(int n, unsigned Mat[10][10]){
     ofstream fisier("Fisier.txt");
-    for(unsigned i = 1; i<=n; i++)
+    for(unsigned i = 0; i<n; i++)
     {
-        for(unsigned j = 1; j<=n; j++)
-            fisier << Mat[i][j] << ' ';
-        fisier << '\n';
+        for(unsigned j = 0; j<n; j++)
+            fisier << Mat[i][j] << " ";
+        fisier << "\n";
     }
     fisier.close();
 }
 
-void AfisareMatriceTast(unsigned Mat[10][10], unsigned n){
-    for(unsigned i = 1; i <=n; i++){
-        for(unsigned j = 1; j <=n; j++)
+void CitesteFisier(unsigned Mat[10][10], unsigned n){
+    ifstream fisier("Fisier.txt");
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; ++j)
+            fisier >> Mat[i][j];
+    }
+    fisier.close();
+}
+
+void AfisareMatrice(unsigned Mat[10][10], unsigned n){
+    for(unsigned i = 0; i <n; i++){
+        for(unsigned j = 0; j <n; j++)
             cout << Mat[i][j]<< ' ';
         cout<< '\n';
     }
@@ -61,21 +65,19 @@ void AfisareMatriceTast(unsigned Mat[10][10], unsigned n){
 
 int main(){
     unsigned int mod;
-    cout << "Graf Orientat?";
-    cin >> orientat;
+    cout<<"Numarul de elemente: "; cin>>n; cout << n;
     cout<<"Aplciatia cu grafuri: \n 1 Citeste elementele din fisier pentru grafuri \n 2 Afisare graful \n 3 Afisati gradul nodului din graf \n 4 Afisati graful partial al grafului \n 5 Afisati subgraful al unui graf \n 6 Afisati graful complet cu n noduri";
     cin >> mod;
     switch (mod)
     {
     case 1:
-        Citeste(n, orientat, Mat);
-        cout << '\n';
-        AfisareMatriceTast(Mat, 3);
+        CitesteTast(n, Mat);
         Scriere(3, Mat);
         //cout << "test";
         break;
     case 2:
-
+        CitesteFisier(Mat, n);
+        AfisareMatrice(Mat, n);
         break;
     case 3:
 
